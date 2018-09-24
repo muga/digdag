6eaa51b59c29f50791470c7f65b6fa5ec6dd5e97
Muga Nishizawa <muga.nishizawa@gmail.com>
2018-09-24 13:42:10 -0700

    Make KubernetesClientConfig simple by StorageManager.extractKeyPrefix method

--- digdag-standards/src/main/java/io/digdag/standards/command/kubernetes/KubernetesClientConfig.java
+++ digdag-standards/src/main/java/io/digdag/standards/command/kubernetes/KubernetesClientConfig.java
@@ -2,31 +2,30 @@ package io.digdag.standards.command.kubernetes;
 
 import io.digdag.client.config.Config;
 import io.digdag.client.config.ConfigException;
+import io.digdag.core.storage.StorageManager;
 
 public class KubernetesClientConfig
 {
+    private static final String KUBERNETES_CLIENT_PARAMS_PREFIX = "agent.command_executor.kubernetes.";
+
     public static KubernetesClientConfig create(final Config systemConfig, final Config requestConfig)
             throws ConfigException
     {
-        if (requestConfig.has("kubernetes")) { // task from request config
+        if (requestConfig.has("kubernetes")) {
+            // from task request config
             return KubernetesClientConfig.createFromTaskRequestConfig(requestConfig.getNested("kubernetes"));
         }
-        else { // from system config
+        else {
+            // from system config
             return KubernetesClientConfig.createFromSystemConfig(systemConfig);
         }
     }
 
     private static KubernetesClientConfig createFromTaskRequestConfig(final Config config)
     {
-        // TODO throw config exception if parameters are missed
-        if (!config.has("master") ||
-                !config.has("certs_ca_data") ||
-                !config.has("oauth_token") ||
-                !config.has("namespace")) {
-            return null;
-        }
-
-        return create(config.get("master", String.class),
+        validateParams(config);
+        return create(config.get("name", String.class, "default"),
+                config.get("master", String.class),
                 config.get("certs_ca_data", String.class),
                 config.get("oauth_token", String.class),
                 config.get("namespace", String.class));
@@ -34,44 +33,59 @@ public class KubernetesClientConfig
 
     private static KubernetesClientConfig createFromSystemConfig(final io.digdag.client.config.Config systemConfig)
     {
-        // TODO throw config exception if parameters are missed
-        final String configKeyPrefix = "agent.command_executor.kubernetes.";
-        if (!systemConfig.has(configKeyPrefix + "master") ||
-                !systemConfig.has(configKeyPrefix + "certs_ca_data") ||
-                !systemConfig.has(configKeyPrefix + "oauth_token") ||
-                !systemConfig.has(configKeyPrefix + "namespace")) {
-            return null;
-        }
-
-        return create(systemConfig.get(configKeyPrefix + "master", String.class),
-                systemConfig.get(configKeyPrefix + "certs_ca_data", String.class),
-                systemConfig.get(configKeyPrefix + "oauth_token", String.class),
-                systemConfig.get(configKeyPrefix + "namespace", String.class));
+        final String name = systemConfig.get(KUBERNETES_CLIENT_PARAMS_PREFIX + "name", String.class, "default");
+        final String keyPrefix = "default".equals(name) ? KUBERNETES_CLIENT_PARAMS_PREFIX : KUBERNETES_CLIENT_PARAMS_PREFIX + name + ".";
+        final Config extracted = validateParams(StorageManager.extractKeyPrefix(systemConfig, keyPrefix));
+        return create(name,
+                extracted.get("master", String.class),
+                extracted.get("certs_ca_data", String.class),
+                extracted.get("oauth_token", String.class),
+                extracted.get("namespace", String.class));
     }
 
-    private static KubernetesClientConfig create(final String master,
+    private static Config validateParams(final Config config)
+    {
+        if (!config.has("master") ||
+                !config.has("certs_ca_data") ||
+                !config.has("oauth_token") ||
+                !config.has("namespace")) {
+            throw new ConfigException("kubernetes config must have master:, certs_ca_data:, oauth_token: and namespace:");
+        }
+        return config;
+    }
+
+    private static KubernetesClientConfig create(final String name,
+            final String master,
             final String certsCaData,
             final String oauthToken,
             final String namespace)
     {
         System.setProperty("kubernetes.auth.tryKubeConfig", "false");
-        return new KubernetesClientConfig(master, namespace, certsCaData, oauthToken);
+        return new KubernetesClientConfig(name, master, namespace, certsCaData, oauthToken);
     }
 
+    private final String name;
     private final String master;
     private final String namespace;
     private final String certsCaData;
     private final String oauthToken;
 
-    private KubernetesClientConfig(final String master,
+    private KubernetesClientConfig(final String name,
+            final String master,
             final String namespace,
             final String certsCaData,
             final String oauthToken)
     {
+        this.name = name;
         this.master = master;
         this.namespace = namespace;
         this.certsCaData = certsCaData;
         this.oauthToken = oauthToken;
+    }
+
+    public String getName()
+    {
+        return this.name;
     }
 
     public String getMaster()
